6198de4cd2a4ededd45d0975996b49d7cd0b6ad2
Oliver Caldwell <olliec87@gmail.com>
2018-06-10 14:45:02 +0100

    Initial attempt at problem 3
    Needs a rethink because it's waaaaay too slow.

---
+++ src/p_0003.clj
@@ -0,0 +1,35 @@
+(ns p-0003)
+
+;; The prime factors of 13195 are 5, 7, 13 and 29.
+;; What is the largest prime factor of the number 600851475143 ?
+
+(defn div-by? [x y]
+  (zero? (mod x y)))
+
+(defn is-prime?* [n]
+  (loop [t (dec n)]
+    (cond
+      (= t 1) true
+      (< t 1) false
+      (div-by? n t) false
+      :else (recur (dec t)))))
+
+(def is-prime? (memoize is-prime?*))
+
+(def primes (filter is-prime? (range)))
+
+(declare prime-factors)
+
+(defn prime-factors* [n]
+  (loop [[prime & primes] primes
+         acc []]
+    (cond
+      (is-prime? n) (conj acc n)
+      (div-by? n prime) (into (conj acc prime) (prime-factors (/ n prime)))
+      :else (recur primes acc))))
+
+(def prime-factors (memoize prime-factors*))
+
+(comment
+  (last (prime-factors 13195)) ;; => 29
+  (last (prime-factors 600851475143)))
