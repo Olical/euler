61545e5916d8e9e65145772ff14b5cc1da5b0cb7
Oliver Caldwell <olliec87@gmail.com>
2018-06-11 22:30:41 +0100

    Completed Q5

--- src/p_0005.clj
+++ src/p_0005.clj
@@ -2,3 +2,23 @@
 
 ;; 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 ;; What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+(defn divides? [x y]
+  (zero? (mod x y)))
+
+(defn divides-evenly-into-range? [n x]
+  (loop [n n]
+    (cond
+      (zero? n) true
+      (divides? x n) (recur (dec n))
+      :else false)))
+
+(defn smallest-divides [n]
+  (loop [x 1]
+    (if (divides-evenly-into-range? n x)
+      x
+      (recur (inc x)))))
+
+(comment
+  (smallest-divides 10)
+  (smallest-divides 20))
