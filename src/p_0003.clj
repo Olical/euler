32b1f5e7cb2d4f4c95d93aa98207b1f88bad4e60
Oliver Caldwell <olliec87@gmail.com>
2018-06-11 21:10:21 +0100

    Ayyyy, got Q3 working fast
    Had to read a LOT of other work and math things though. Would never have
    got this without peeking at a bunch of different ideas and approaches to
    similar things.

--- src/p_0003.clj
+++ src/p_0003.clj
@@ -3,25 +3,22 @@
 ;; The prime factors of 13195 are 5, 7, 13 and 29.
 ;; What is the largest prime factor of the number 600851475143 ?
 
-(defn div-by? [x y]
-  (zero? (mod x y)))
+(defn divides? [n d]
+  (zero? (mod n d)))
 
-(defn is-prime? [n]
-  (loop [t (dec n)]
-    (cond
-      (= t 1) true
-      (< t 1) false
-      (div-by? n t) false
-      :else (recur (dec t)))))
-
-(def primes (filter is-prime? (range)))
+(defn divide-down [n p d]
+  (if (divides? n d)
+    (recur (/ n d) d d)
+    [n p]))
 
 (defn largest-prime-factor [n]
-  (loop [[prime & primes] primes]
-    (cond
-      (is-prime? n) n
-      (div-by? n prime) (largest-prime-factor (/ n prime))
-      :else (recur primes))))
+  (let [[n p] (divide-down n -1 2)
+        limit (-> (Math/sqrt n) (inc) (int))]
+    (loop [[n p] [n p]
+           odds (range 3 limit 2)]
+      (if-let [odd (first odds)]
+       (recur (divide-down n p odd) (rest odds))
+        (max n p)))))
 
 (comment
   (largest-prime-factor 13195)
