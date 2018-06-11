357490262f56398cce8bdfa2c2b92c6df476e47f
Oliver Caldwell <olliec87@gmail.com>
2018-06-11 21:37:41 +0100

    Q4 complete

---
+++ src/p_0004.clj
@@ -0,0 +1,16 @@
+(ns p-0004)
+
+;; A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+;; Find the largest palindrome made from the product of two 3-digit numbers.
+
+(defn palindrome? [n]
+  (let [cs (seq (str n))]
+    (= cs (reverse cs))))
+
+(let [r (range 100 1000)
+      ps (for [x r
+               y r
+               :let [p (* x y)]
+               :when (palindrome? p)]
+           p)]
+  (reduce max -1 ps))
